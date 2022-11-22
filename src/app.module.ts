787feb69b319dd47ce2995194b4900b7c7e40970
MatheusVict <matheusvictorhenrique@gmail.com>
2022-11-22 10:32:19 -0300

    Database config and create service and controllers for users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASS,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
