706bbd775737b24290b5bbe1afc2f1ce012764a4
MatheusVict <matheusvictorhenrique@gmail.com>
2022-11-22 09:50:17 -0300

    First commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+    it('should controller and service defined', () => {
+      expect(appController).toBeDefined();
+      expect(appService).toBeDefined();
+    });
+  });
+});
